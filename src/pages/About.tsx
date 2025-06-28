f3ef753d6ebc896053db892dd14ae2895ff2c810
Dijana Pavlovic <dijana.pavlovic992@gmail.com>
2025-06-28 16:38:45 +0200

    fix about page conflict

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,7 @@
 const About = () => {
   return (
-<<<<<<< Updated upstream
-    <main className="min-h-screen bg-white">
-      {/* <div className="container mx-auto px-6 py-32">
-        <div className="max-w-4xl mx-auto">
-          <h1 className="text-4xl font-bold text-gray-900 mb-8 text-center">About Us</h1>
-          <div className="flex justify-center">
-            <BusinessCard />
-          </div>
-        </div>
-      </div> */}
-=======
     <main className="min-h-screen pt-[42px]">
       <div className="container mx-auto px-6"></div>
->>>>>>> Stashed changes
     </main>
   );
 };
